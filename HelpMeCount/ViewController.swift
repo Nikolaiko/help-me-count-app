f2dd10fb09ab26e505fa847213d7ddce6644f157
Nikolai Baklanov <nikolaiko@mail.ru>
2025-10-22 12:37:32 +0600

    Initial project

---
+++ HelpMeCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelpMeCount
+//
+//  Created by Nikolai Baklanov on 22.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
